51cae411ab606204934252f7de272f961c24a831
Ariyan13186576 <nazmuss024@gmail.com>
2024-07-17 09:22:41 +0600

    sd

--- style.js
+++ style.js
@@ -1,3 +1,14 @@
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.optn').classList.toggle('show');
 });
+
+
+      function openNav() {
+        document.getElementById("myNav").style.width = "60%";
+
+        document.getElementById("myNav").style.width = "70%";
+      }
+      
+      function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+      }
